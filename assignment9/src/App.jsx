8174a34a503e2063465ca6f802ea58e98a5e284c
Vigneshwar Balakrishnan <balakrishnan.vi@northeastern.edu>
2026-04-13 16:03:02 -0400

    wired redux provider and role based routing in app

--- assignment9/src/App.jsx
+++ assignment9/src/App.jsx
@@ -1,5 +1,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import ProtectedRoute from './components/ProtectedRoute';
+import AdminRoute from './components/AdminRoute';
+import EmployeeRoute from './components/EmployeeRoute';
 import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Home from './pages/Home';
@@ -7,57 +9,56 @@ import About from './pages/About';
 import JobListings from './pages/JobListings';
 import CompanyShowcase from './pages/CompanyShowcase';
 import Contact from './pages/Contact';
+import EmployeesPage from './pages/admin/EmployeesPage';
+import AddJobPage from './pages/admin/AddJobPage';
+
+const withNav = (Page) => (
+  <>
+    <Navbar />
+    <Page />
+  </>
+);
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+
+      {/* Admin-only routes */}
+      <Route
+        path="/admin/employees"
+        element={<AdminRoute>{withNav(EmployeesPage)}</AdminRoute>}
+      />
+      <Route
+        path="/add-job"
+        element={<AdminRoute>{withNav(AddJobPage)}</AdminRoute>}
+      />
+
+      {/* Employee-only routes */}
+      <Route
+        path="/jobs"
+        element={<EmployeeRoute>{withNav(JobListings)}</EmployeeRoute>}
+      />
+
+      {/* Both roles (ProtectedRoute) */}
       <Route
         path="/"
-        element={
-          <ProtectedRoute>
-            <Navbar />
-            <Home />
-          </ProtectedRoute>
-        }
+        element={<ProtectedRoute>{withNav(Home)}</ProtectedRoute>}
       />
       <Route
         path="/about"
-        element={
-          <ProtectedRoute>
-            <Navbar />
-            <About />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/jobs"
-        element={
-          <ProtectedRoute>
-            <Navbar />
-            <JobListings />
-          </ProtectedRoute>
-        }
+        element={<ProtectedRoute>{withNav(About)}</ProtectedRoute>}
       />
       <Route
         path="/companies"
-        element={
-          <ProtectedRoute>
-            <Navbar />
-            <CompanyShowcase />
-          </ProtectedRoute>
-        }
+        element={<ProtectedRoute>{withNav(CompanyShowcase)}</ProtectedRoute>}
       />
       <Route
         path="/contact"
-        element={
-          <ProtectedRoute>
-            <Navbar />
-            <Contact />
-          </ProtectedRoute>
-        }
+        element={<ProtectedRoute>{withNav(Contact)}</ProtectedRoute>}
       />
-      <Route path="*" element={<Navigate to="/" replace />} />
+
+      <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
 }
--- assignment9/src/main.jsx
+++ assignment9/src/main.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import App from './App.jsx';
+import store from './store/store';
 import theme from './theme';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
